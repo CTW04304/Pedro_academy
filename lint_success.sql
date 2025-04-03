c056b2fc22c4c3b50268b9aefbd99284006af55c
ctw04304 <ctw04304@criticaltechworks.com>
2025-04-03 19:47:47 +0100

    lint sql files workflow

--- lint_success.sql
+++ lint_success.sql
@@ -1,6 +1,5 @@
-SELECT column1, column2 
-FROM table_name 
-WHERE column1 = 'some_value' 
-AND column2 = 123;  -- Ensure spacing is correct
-
--- Add a trailing newline at the end of the file
+SELECT 
+    column1, 
+    column2 
+FROM 
+    table_name;  -- Ensure no trailing whitespace here
